67dd6d1430cea53b94f394117c9eb6f5b869a381
siuramka <marius.kamarauskas2001@gmail.com>
2023-01-30 15:42:56 +0200

    decouple

---
+++ socketsClient.js
@@ -0,0 +1,36 @@
+const app = require("./expressServer")
+const server = require("http").createServer(app);
+const eventEmitter = require("./twitch/twitchClient");
+const config = require("./configs/config")
+const tts = require("./tts/tts")
+const PORT = config.PORT;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+eventEmitter.on('botMessage', message => {
+  tts.getAudioUrl(message).then((resp) => {
+    console.log(resp)
+    eventEmitter.emit("sendAudioUrl", resp.speak_url);
+  })
+  .catch((resp) =>{
+    console.log("Error fetching TTS API!")
+    console.error(resp.json())
+  })
+});
+
+io.on("connection", function (socket) {
+  console.log("socket.io connnected...")
+  eventEmitter.on("sendAudioUrl", (audioLink) => {
+    socket.send(audioLink);
+    socket.emit("data", audioLink);
+  });
+});
+
+server.listen(PORT, function () {
+  console.log("Socket.IO server [PORT] " + PORT);
+});
--- twitch/twitchClient.js
+++ twitch/twitchClient.js
@@ -9,27 +9,31 @@ const USERNAME_OAUTH = config.USERNAME_OAUTH;
 const CHANNEL = config.CHANNEL;
 
 const options = {
-    options: {
-      debug: false
-    },
-    connection: {
-      reconnect: true
-    },
-    identity: {
-      username: USERNAME_OAUTH,
-      password: OAUTH
-    },
-    channels: [CHANNEL]
-  };
-  
-  const client = new tmi.Client(options);
-  client.connect();
-  
-  client.on("chat", (channel, user, message, self) => {
-    if (user.username === CHANNEL) {
-      console.log(`${user.username}: ${message}`);
-      eventEmitter.emit("botMessage", message);
-    }
-  });
+  options: {
+    debug: false
+  },
+  connection: {
+    reconnect: true
+  },
+  identity: {
+    username: USERNAME_OAUTH,
+    password: OAUTH
+  },
+  channels: [CHANNEL]
+};
+
+const client = new tmi.Client(options);
+client.connect();
+
+/*
+  Emits event when message is from user CHANNEL
+*/
+
+client.on("chat", (channel, user, message, self) => {
+  if (user.username === CHANNEL) {
+    console.log(`${user.username}: ${message}`);
+    eventEmitter.emit("botMessage", message);
+  }
+});
 
 module.exports = eventEmitter;
