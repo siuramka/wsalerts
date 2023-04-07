0a7b646c2fda76de1582ffd35d35a077ba362108
siuramka <marius.kamarauskas2001@gmail.com>
2023-04-07 23:28:22 +0300

    Fix promise returns/ add one after reconnect

--- twitch/twitchClient.ts
+++ twitch/twitchClient.ts
@@ -3,58 +3,54 @@ import { config } from "../configs/config";
 import { eventEmitter } from "../events/eventsHandler";
 import { CommandFactory, parsedCommand } from "./commandFactory";
 
-const USERNAME_OAUTH = config.USERNAME_OAUTH
-const AUTHORIZED_USERS = config.AUTHORIZED_USERS
+const USERNAME_OAUTH = config.USERNAME_OAUTH;
+const AUTHORIZED_USERS = config.AUTHORIZED_USERS;
 
-const EVENTS = {
-
-}
+const EVENTS = {};
 
 interface ITwitchClient {
-  getOptions: () => tmi.Options
-  connect: () => Promise<void> 
-  disconnect: () => Promise<void> 
-  reconnect: () => Promise<void> 
-  updateOptions: (options: tmi.Options) => void
+  getOptions: () => tmi.Options;
+  connect: () => Promise<[string,number]>;
+  disconnect: () => Promise<[string,number]>;
+  reconnect: () => Promise<string>;
+  updateOptions: (options: tmi.Options) => void;
 }
 
-interface IConfigTwitchClient {
-
-}
+interface IConfigTwitchClient {}
 
 export class TwitchClient implements ITwitchClient {
-  private client: tmi.Client
-  private options: tmi.Options
-  private chatEventsHandler: IChatEventsHandler
+  private client: tmi.Client;
+  private options: tmi.Options;
+  private chatEventsHandler: IChatEventsHandler;
 
   constructor() {
     // Could insert config into params
-    this.setupOptions()
-    this.client = this.initClient(this.getOptions())
+    this.setupOptions();
+    this.client = this.initClient(this.getOptions());
     //should I add this here or do it the other way arround: add GetClient, and get client in the handler class
-    this.chatEventsHandler = new ChatEventsHandler(this.client)
-
+    this.chatEventsHandler = new ChatEventsHandler(this.client);
   }
 
   private initClient(options: tmi.Options): tmi.Client {
-    return new tmi.Client(options)
+    return new tmi.Client(options);
   }
 
-  public async connect(): Promise<void> {
-    await this.client.connect();
+  public async connect(): Promise<[string,number]> {
+    return await this.client.connect();
   }
 
-  public async disconnect(): Promise<void>  {
-    await this.client.disconnect()
+  public async disconnect(): Promise<[string,number]> {
+    return await this.client.disconnect();
   }
 
-  public async reconnect(): Promise<void>  {
+  public async reconnect(): Promise<string> {
     await this.disconnect();
     await this.connect();
+    return "Reconnected to twitch client!"
   }
 
   public updateOptions(options: tmi.Options): void {
-    this.options = options
+    this.options = options;
   }
 
   /**
@@ -63,27 +59,26 @@ export class TwitchClient implements ITwitchClient {
   private setupOptions(): void {
     const OAUTH = config.OAUTH;
     const USERNAME_OAUTH = config.USERNAME_OAUTH;
-    const CHANNEL = config.CHANNEL
-    this.options =  {
+    const CHANNEL = config.CHANNEL;
+    this.options = {
       options: {
-        debug: true
+        debug: true,
       },
       connection: {
-        reconnect: true
+        reconnect: true,
       },
       identity: {
         username: USERNAME_OAUTH,
-        password: OAUTH
+        password: OAUTH,
       },
-      channels: CHANNEL
+      channels: CHANNEL,
     };
   }
 
   public getOptions(): tmi.Options {
     return this.options;
-  };
+  }
 }
-
 
 /*
   I could add a method to twitch client class, and do client.on event handling there, but thats not what the class is for
@@ -91,58 +86,58 @@ export class TwitchClient implements ITwitchClient {
 */
 
 interface IChatEventsHandler {
-  setupChatListener: () => void
+  setupChatListener: () => void;
 }
 
 class ChatEventsHandler implements IChatEventsHandler {
-  private _client: tmi.Client
+  private _client: tmi.Client;
 
   constructor(client: tmi.Client) {
     this._client = client;
-    this.setupChatListener()
+    this.setupChatListener();
   }
   public setupChatListener(): void {
     this._client.on("chat", (channel, user, message, self) => {
-      const commandName = message.split(" ")[0]
-      console.log("================= change later ====================")
-      const isUserAuthorized = true//AUTHORIZED_USERS?.includes(user.username.toLowerCase())
+      const commandName = message.split(" ")[0];
+      console.log("================= change later ====================");
+      const isUserAuthorized = true; //AUTHORIZED_USERS?.includes(user.username.toLowerCase())
       try {
-    //logic for this:https://www.baeldung.com/java-replace-if-statements
-    //https://refactoring.guru/design-patterns/command/typescript/example
-    //command pattern
+        //logic for this:https://www.baeldung.com/java-replace-if-statements
+        //https://refactoring.guru/design-patterns/command/typescript/example
+        //command pattern
         if (user.username === USERNAME_OAUTH) {
           console.log(`${user.username}: ${message}`);
           eventEmitter.emit("synthesizeAudioUberduck", message);
         }
-        if(isUserAuthorized){
-          const targetCommand = CommandFactory.getOperation(commandName)
-          targetCommand?.parse(user, message)
-          targetCommand?.handle()
+        
+        if (isUserAuthorized) {
+          const targetCommand = CommandFactory.getOperation(commandName);
+          targetCommand?.parse(user, message);
+          targetCommand?.handle();
         }
-  
       } catch (error) {
-        console.log(`Error in chat parsing! => `)
-        console.log(error)
+        console.log(`Error in chat parsing! => `);
+        console.log(error);
       }
     });
-  }  
+  }
   // public setupChatListener(): void {
   //   this._client.on("chat", (channel, user, message, self) => {
   //     const commandName = message.split(" ")[0]
   //     console.log("================= change later ====================")
   //     const isUserAuthorized = true//AUTHORIZED_USERS?.includes(user.username.toLowerCase())
   //     try {
-    
+
   //       if (user.username === USERNAME_OAUTH) {
   //         console.log(`${user.username}: ${message}`);
   //         eventEmitter.emit("synthesizeAudioUberduck", message);
   //       }
-    
+
   //       if(commandName === "!tts" && isUserAuthorized) {
   //           const messageInfo = commandHandler.parseChatCommandMessageTts(user, message)
   //           commandHandler.handleCommandTts(messageInfo)
   //       }
-        
+
   //       if(commandName == "!ttsv" && isUserAuthorized) {
   //         const messageInfo = commandHandler.parseChatCommandMessageTtsv(user, message)
   //         commandHandler.handleCommandTtsv(messageInfo)
@@ -150,7 +145,7 @@ class ChatEventsHandler implements IChatEventsHandler {
   //       if(commandName == "!tts11" && isUserAuthorized) {
   //         const messageInfo = commandHandler.parseChatCommandMessageTts(user, message)
   //         commandHandler.handleCommandTts11(messageInfo)
-    
+
   //       }
   //     } catch (error) {
   //       console.log(`Error in chat parsing! => `)
@@ -159,6 +154,3 @@ class ChatEventsHandler implements IChatEventsHandler {
   //   });
   // }
 }
-
-
-
