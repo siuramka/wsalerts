cbc2e052319a05cf0d033caaa226626cf4c0734f
siuramka <marius.kamarauskas2001@gmail.com>
2023-04-10 01:30:58 +0300

    config interfaces for different congigs + export data of em from env

--- configs/config.ts
+++ configs/config.ts
@@ -1,24 +1,41 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-interface Config {
+//create multiple configs for different stuff
+// https://www.typescripttutorial.net/typescript-tutorial/typescript-extend-interface/
+// instead of I1 | I2 | Ix... 
+export interface ApiConfig extends TwitchConfig, ElevenlabsConfig, UberduckConfig {}
+
+interface TwitchConfig {
   PORT: string | undefined;
   OAUTH: string | undefined;
   USERNAME_OAUTH: string | undefined;
   CHANNEL: string[] | undefined;
-  API_KEY: string | undefined;
-  API_SECRET: string | undefined;
-  XI_API_KEY: string | undefined;
   AUTHORIZED_USERS: string[] | undefined;
 }
 
-export const config: Config = {
+interface ElevenlabsConfig {
+  XI_API_KEY: string | undefined;
+}
+
+interface UberduckConfig {
+  API_KEY: string | undefined;
+  API_SECRET: string | undefined;
+}
+
+export const UberduckConfig = {
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET
+}
+
+export const ElevenlabsConfig: ElevenlabsConfig  = {
+  XI_API_KEY: process.env.XI_API_KEY
+}
+
+export const TwitchConfig: TwitchConfig = {
   PORT: process.env.PORT,
   OAUTH: process.env.OAUTH,
   USERNAME_OAUTH: process.env.USERNAME_OAUTH,
   CHANNEL: process.env.CHANNEL?.split(", "),
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET,
-  XI_API_KEY: process.env.XI_API_KEY,
   AUTHORIZED_USERS: process.env.AUTHORIZED_USERS?.split(", "),
 };
