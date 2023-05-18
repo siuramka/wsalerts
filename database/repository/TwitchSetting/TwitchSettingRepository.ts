194f865b5f8a3428a3ab21eeadbb27ae4b0f87d2
siuramka <marius.kamarauskas2001@gmail.com>
2023-05-18 17:39:03 +0300

    Don't need IWritable for TwitchSettingRepository

--- database/repository/TwitchSetting/TwitchSettingRepository.ts
+++ database/repository/TwitchSetting/TwitchSettingRepository.ts
@@ -15,23 +15,23 @@ class TwitchSettingRepository implements IReadable, IWritable {
   public async get(id: number): Promise<TwitchSetting | null> {
     return await this.prisma.twitchSetting.findUnique({ where: { id } });
   }
-  //IWritable
-  public async create(entity: TwitchSetting): Promise<TwitchSetting> {
-    return await this.prisma.twitchSetting.create({ data: entity });
-  }
-  public async update(
-    id: number,
-    entity: TwitchSetting
-  ): Promise<TwitchSetting> {
-    return await this.prisma.twitchSetting.update({
-      where: { id },
-      data: entity,
-    });
-  }
-  public async delete(id: number) {
-    return await this.prisma.twitchSetting.delete({ where: { id } });
-  }
-  public async deleteAll() {
-    return await this.prisma.twitchSetting.deleteMany();
-  }
+//   //IWritable
+//   public async create(entity: TwitchSetting): Promise<TwitchSetting> {
+//     return await this.prisma.twitchSetting.create({ data: entity });
+//   }
+//   public async update(
+//     id: number,
+//     entity: TwitchSetting
+//   ): Promise<TwitchSetting> {
+//     return await this.prisma.twitchSetting.update({
+//       where: { id },
+//       data: entity,
+//     });
+//   }
+//   public async delete(id: number) {
+//     return await this.prisma.twitchSetting.delete({ where: { id } });
+//   }
+//   public async deleteAll() {
+//     return await this.prisma.twitchSetting.deleteMany();
+//   }
 }
