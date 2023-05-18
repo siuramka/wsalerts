e2838ba581dc0e5fdaa617b28926e9b22f6a391e
siuramka <marius.kamarauskas2001@gmail.com>
2023-05-18 19:06:27 +0300

    Remove unnecessary methods and interfaces

--- database/repository/TwitchSetting/TwitchSettingRepository.ts
+++ database/repository/TwitchSetting/TwitchSettingRepository.ts
@@ -3,35 +3,13 @@ import { IReadable } from "../base/interface/IReadable";
 import { IWritable } from "../base/interface/IWritable";
 import DatabaseConfig from "../../DatabaseConfig";
 
-class TwitchSettingRepository implements IReadable {
+export class TwitchSettingRepository {
   private prisma: PrismaClient;
   constructor() {
     this.prisma = DatabaseConfig.getInstance().getPrismaClient();
   }
-  //IReadable
-  public async getAll(): Promise<Array<TwitchSetting> | null> {
-    return await this.prisma.twitchSetting.findMany();
+
+  public async getFirst(): Promise<TwitchSetting | null> {
+    return await this.prisma.twitchSetting.findFirst();
   }
-  public async get(id: number): Promise<TwitchSetting | null> {
-    return await this.prisma.twitchSetting.findUnique({ where: { id } });
-  }
-//   //IWritable
-//   public async create(entity: TwitchSetting): Promise<TwitchSetting> {
-//     return await this.prisma.twitchSetting.create({ data: entity });
-//   }
-//   public async update(
-//     id: number,
-//     entity: TwitchSetting
-//   ): Promise<TwitchSetting> {
-//     return await this.prisma.twitchSetting.update({
-//       where: { id },
-//       data: entity,
-//     });
-//   }
-//   public async delete(id: number) {
-//     return await this.prisma.twitchSetting.delete({ where: { id } });
-//   }
-//   public async deleteAll() {
-//     return await this.prisma.twitchSetting.deleteMany();
-//   }
 }
