8569296333be02259831d00b613fdcf29dd65a2f
siuramka <marius.kamarauskas2001@gmail.com>
2023-06-07 01:00:55 +0300

    table frontend buttons, handling

--- tts-frontend/src/pages/Settings/TtsSettings.tsx
+++ tts-frontend/src/pages/Settings/TtsSettings.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormHelperText, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Box, Button, FormControl, FormHelperText, MenuItem, Select, SelectChangeEvent, Stack, Typography } from "@mui/material";
 import axios from "axios";
 import { useState, useContext, useEffect } from "react";
 import { LoaderContext } from "../../context/LoaderContext";
@@ -6,16 +6,33 @@ import { VoiceRespose } from "../../types/models/VoiceResponse";
 import { AuthContext } from "../../context/AuthContext";
 import { ProviderResponse } from "../../types/models/provider/ProvidersRespone";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
+import DoneIcon from '@mui/icons-material/Done';
+import DeleteIcon from '@mui/icons-material/Delete';
 const TtsSettings = () => {
 
   const { user } = useContext(AuthContext);
+  const [editable, setEditable] = useState(false);
+  const [deletable, setDeletable] = useState(false);
   const [providers, setProviders] = useState<ProviderResponse[]>([]);
   const [selectedProvider, setSelectedProvider] = useState<ProviderResponse>();
   const { loading, setLoaderHandler } = useContext(LoaderContext);
   const headers = { Authorization: user?.token };
   const [voices, setVoices] = useState<VoiceRespose[]>([]);
   const [select, setSelect] = useState('');
+
+  const handleSave = () => {
+
+  }
+
+  const handleEditable = () => {
+    setEditable(true) // no need for if check since react won't re render if the same value pass
+  }
+
+  const handleDeletable = () => {
+    setDeletable(!deletable)
+  }
 
   const handleChange = (event: SelectChangeEvent) => {
     setSelect(event.target.value);
@@ -31,17 +48,18 @@ const TtsSettings = () => {
 
   const getVoices = () => {
     return axios.get<VoiceRespose[]>(`/api/voice/provider/${selectedProvider?.name}`, { headers });
-
   };
 
   const columns: GridColDef[] = [
-    { field: "id", headerName: "Voice ID", width: 70 },
-    { field: "displayName", headerName: "Display Name" },
-    { field: "name", headerName: "API Name" },
+    { field: "id", headerName: "Voice ID", width: 150, editable: editable },
+    { field: "displayName", headerName: "Display Name", width: 250, editable: editable },
+    { field: "name", headerName: "API Name", width: 250, editable: editable },
     {
       field: "selected",
       type: "boolean",
       headerName: "Selected",
+      width: 150,
+      editable: editable
     },
   ];
 
@@ -57,9 +75,8 @@ const TtsSettings = () => {
         })
       })
 
-
   }, []);
-  
+
   useEffect(() => {
     getVoices().then((voicesData) => {
       setVoices(voicesData.data);
@@ -72,23 +89,49 @@ const TtsSettings = () => {
     <>
       {loading ? <></> :
         <>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <Select
-              value={select}
-              onChange={handleChange}
-              displayEmpty
-              inputProps={{ 'aria-label': 'Without label' }}
-            >
-              {
-                providers.map((provider) =>
-                  <MenuItem value={provider.id.toString()}>{provider.name}</MenuItem>
-                )
-              }
-            </Select>
-          </FormControl>
-          <div style={{ height: "50vh", width: "100%" }}>
-            <DataGrid rows={voices} columns={columns} />
-          </div>
+          <Box sx={{ pt: 2 }}>
+            <Typography variant="h5" gutterBottom>
+              Text-to-Speech Settings
+            </Typography>
+            <Box sx={{ pb: 2, pt: 1, display: "flex", justifyContent: "space-between" }}>
+              <FormControl color="success">
+                <FormHelperText sx={{ color: "#fff" }}>Active TTS API</FormHelperText>
+                <Select
+                  value={select}
+                  onChange={handleChange}
+                  displayEmpty
+                  disabled={!editable}
+                >
+                  {
+                    providers.map((provider) =>
+                      <MenuItem value={provider.id.toString()}>{provider.name}</MenuItem>
+                    )
+                  }
+                </Select>
+              </FormControl>
+              <Stack spacing={2} direction="row" sx={{ p: 2, pr: 0 }}>
+                <Button variant="outlined" startIcon={<AddIcon />}>Add</Button>
+                <Button variant="contained" startIcon={<SettingsIcon />} onClick={handleEditable}>Edit</Button>
+                {
+                  deletable ?
+                    <>
+                      <Button color="error" variant="contained" startIcon={<DoneIcon />} onClick={handleDeletable}>Confirm delete</Button>
+                    </> :
+                    <>
+                      <Button variant="contained" startIcon={<DeleteIcon />} onClick={handleDeletable}>Delete</Button>
+                    </>
+                }
+
+              </Stack>
+            </Box>
+            <div style={{ height: "50vh", width: "100%" }}>
+              <DataGrid checkboxSelection={deletable} editMode="row" rows={voices} columns={columns} />
+            </div>
+            <Box sx={{ pt:2, display: "flex", justifyContent: "right" }}>
+              <Button color="success" variant="outlined" startIcon={<DeleteIcon />} onClick={handleSave}>Save changes</Button>
+            </Box>
+          </Box>
+
         </>
 
       }
