d3f06a3f7d1d566b0b3f3e4295a4b4e2185a1e26
siuramka <marius.kamarauskas2001@gmail.com>
2023-06-15 14:23:37 +0300

    working docker compose tts with db

--- tts-ts/scripts.sh
+++ tts-ts/scripts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # mkdir -p prisma/migrations/init
 # npx prisma migrate diff --preview-feature --from-empty --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/init/migration.sql
-npx prisma migrate resolve --applied init
-npx prisma migrate dev --name init && npm run start
+# npx prisma migrate resolve --applied init
+# npx prisma migrate dev --name init && npm run start
+npx prisma migrate deploy
+npx prisma db seed
+npm run start
